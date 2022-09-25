8b10e6763099d2753fae210f45b1cf492dbd9a7f
Thiago Gomes <thiagosiqgomes@gmail.com>
2022-09-25 19:25:30 -0300

    chore: add super permission to user

--- init/01.sql
+++ init/01.sql
@@ -3,3 +3,5 @@ GRANT ALL ON `first_database`.* TO 'user'@'%';
 
 CREATE DATABASE IF NOT EXISTS `second_database`;
 GRANT ALL ON `second_database`.* TO 'user'@'%';
+
+GRANT SUPER ON *. * TO 'user'@'%'
