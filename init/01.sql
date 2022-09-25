01d159baa402bbbd411583cbb87b3fb0337fd654
Thiago Gomes <thiagosiqgomes@gmail.com>
2022-09-25 14:59:47 -0300

    feat: add docker compose to create databases

---
+++ init/01.sql
@@ -0,0 +1,5 @@
+CREATE DATABASE IF NOT EXISTS `frst_databse`;
+GRANT ALL ON `frst_databse`.* TO 'user'@'%';
+
+CREATE DATABASE IF NOT EXISTS `second_dabase`;
+GRANT ALL ON `second_dabase`.* TO 'user'@'%';
