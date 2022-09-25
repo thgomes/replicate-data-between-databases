a5718b3f50f24b8e014cbd0fe384f525f192bb5b
Thiago Gomes <thiagosiqgomes@gmail.com>
2022-09-25 16:34:23 -0300

    fix: adjust databases name

--- init/01.sql
+++ init/01.sql
@@ -1,5 +1,5 @@
-CREATE DATABASE IF NOT EXISTS `frst_databse`;
-GRANT ALL ON `frst_databse`.* TO 'user'@'%';
+CREATE DATABASE IF NOT EXISTS `first_database`;
+GRANT ALL ON `first_database`.* TO 'user'@'%';
 
-CREATE DATABASE IF NOT EXISTS `second_dabase`;
-GRANT ALL ON `second_dabase`.* TO 'user'@'%';
+CREATE DATABASE IF NOT EXISTS `second_database`;
+GRANT ALL ON `second_database`.* TO 'user'@'%';
